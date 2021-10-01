8bfe106c7eda805808642e692f1feabc0e9d5c6d
vishnuakhil333 <56473590+vishnuakhil333@users.noreply.github.com>
2021-10-01 17:48:50 +0530

    modified the middelware

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const sqlite3 = require("sqlite3");
 const app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://29eng.csb.app/"
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://29eng.csb.app/"
+//   ); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+  res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'DELETE,GET,PATCH,POST,PUT',
+        'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+    });
   next();
 });
 
